e07fe6aa952ab6d66071c4f0a4ff608b1f8f6c24
Camila Ramos <camiinthisthang@Camilas-MacBook-Pro-3.local>
2022-06-27 00:47:24 -0400

    rainbowkit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,42 @@
 import '../styles/globals.css'
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import {
+  chain,
+  configureChains,
+  createClient,
+  WagmiConfig,
+} from 'wagmi';
+import { infuraProvider } from 'wagmi/providers/infura'
+
+const infuraId = process.env.NEXT_PUBLIC_INFURA_ID
+
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon],
+  [infuraProvider({ infuraId })],
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'web3-frontend-starter',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+            <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
 }
 
 export default MyApp
